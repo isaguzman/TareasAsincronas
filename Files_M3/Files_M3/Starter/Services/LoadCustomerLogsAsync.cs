22632e7171a93591eef0c1d75a95a4e94222f2c5
isaguzman <s23120163@alumnos.itsur.edu.mx>
2025-06-22 22:52:06 -0600

    Corrección de archivo

---
+++ Files_M3/Files_M3/Starter/Services/LoadCustomerLogsAsync.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Files_M3;
+
+public class LoadCustomerLogsAsync
+{
+    public static async Task ReadCustomerDataAsync(Bank bank)
+    {
+        string configDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Config");
+        string accountsDirectoryPath = Path.Combine(configDirectoryPath, "Accounts");
+        string transactionsDirectoryPath = Path.Combine(configDirectoryPath, "Transactions");
+
+        await JsonRetrievalAsync.LoadAllCustomersAsync(bank, configDirectoryPath, accountsDirectoryPath, transactionsDirectoryPath);
+    }
+}
